dfcc7f8273510a8ba88b87281be460f6b9033268
Rajal Kumar <rajalkumar@protonmail.com>
2025-06-26 14:19:13 +1200

    fix: fixed up controller structure

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,52 +1,57 @@
-class Api::V1::ProjectsController < Api::V1::BaseController
-  def index
-    @projects = Project.all
-    render json: @projects, status: :ok
-  end
+module Api
+  module V1
+    class ProjectsController < ApplicationController
+      def index
+        @projects = Project.all
+        render json: @projects, status: :ok
+      end
 
-  def show
-    @project = Project.find(params[:id])
-    render json: @project, status: :ok
-  rescue ActiveRecord::RecordNotFound
-    render json: { error: "Project not found" }, status: :not_found
-  end
+      def show
+        @project = Project.find(params[:id])
+        render json: @project, status: :ok
+      rescue ActiveRecord::RecordNotFound
+        render json: { error: "Project not found" }, status: :not_found
+      end
 
-  def create
-    project = Project.new(project_params)
+      def create
+        project = Project.new(project_params)
 
-    if project.save
-      render json: project, status: :created
-    else
-      render json: { errors: project.errors.full_messages }, status: :unprocessable_entity
+        if project.save
+          render json: project, status: :created
+        else
+          render json: { errors: project.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        project = Project.find(params[:id])
+
+        if project.update(project_params)
+          render json: project, status: :ok
+        else
+          render json: { errors: project.errors.full_messages }, status: :unprocessable_entity
+        end
+      rescue ActiveRecord::RecordNotFound
+        render json: { error: "Project not found" }, status: :not_found
+      end
+
+      def destroy
+      project = Project.find(params[:id])
+      project.destroy!
+
+        render json: { message: "Project deleted successfully" }, status: :ok
+      rescue ActiveRecord::RecordNotDestroyed => e
+        render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+      rescue ActiveRecord::RecordNotFound
+        render json: { error: "Project not found" }, status: :not_found
+      end
+
+      private
+
+      def project_params
+      params.require(:project).permit(:title, :description, :status, :technology_stack, :repository_url, :live_url, :notes)
+      end
     end
-  end
-
-  def update
-    project = Project.find(params[:id])
-
-    if project.update(project_params)
-      render json: project, status: :ok
-    else
-      render json: { errors: project.errors.full_messages }, status: :unprocessable_entity
-    end
-  rescue ActiveRecord::RecordNotFound
-    render json: { error: "Project not found" }, status: :not_found
-  end
-
-  def destroy
-   project = Project.find(params[:id])
-   project.destroy!
-
-    render json: { message: "Project deleted successfully" }, status: :ok
-  rescue ActiveRecord::RecordNotDestroyed => e
-    render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
-  rescue ActiveRecord::RecordNotFound
-    render json: { error: "Project not found" }, status: :not_found
-  end
-
-  private
-
-  def project_params
-   params.require(:project).permit(:title, :description, :status, :technology_stack, :repository_url, :live_url, :notes)
   end
 end
+
