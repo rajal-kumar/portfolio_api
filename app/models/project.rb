7d3c44167e1ef4dd60df4a3f685040bc75aa0826
Rajal Kumar <rajalkumar@protonmail.com>
2025-06-26 20:42:16 +1200

    feat: Add validations for Project model attributes

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,9 @@
 class Project < ApplicationRecord
+  STATUS = ["pending", "in_progress", "complete"]
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :status, presence: true, inclusion: { in: STATUS, message: "%{value} is not a valid status" }
+  validates :technology_stack, presence: true
+  validates :repository_url, presence: true
 end
